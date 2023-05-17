4ebdd72826b9da97bcc9e2870ce589fb8578d897
samezzz <95895530+samezzz@users.noreply.github.com>
2023-05-17 05:52:17 +0000

    Fixed footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,13 +7,13 @@ export default function Footer() {
     <section className="flex flex-col text-gray-100">
       <div className="flex flex-row justify-center align-middle gap-4 text-white text-4xl lg:text-5xl">
         <Link href="https://www.linkedin.com/in/samuel-essilfie-274684252/">
-          <FaLinkedinIn />
+          <FaLinkedinIn className="w-8 h-8 text-[#0072b1]" />
         </Link>
         <Link href="https://github.com/samezzz">
-          <FaGithub />
+          <FaGithub className="w-8 h-8" />
         </Link>
         <Link href="https://twitter.com/_Samess">
-          <FaTwitter />
+          <FaTwitter className="w-8 h-8 text-[#1DA1F2]" />
         </Link>
       </div>
       <div className="mx-auto my-6 text-center text-gray-400 ">&copy; 2023 MINDSHARE MEMOIRS. All rights reserved.
